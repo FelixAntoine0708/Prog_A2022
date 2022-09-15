6f7aa32c500814442ac643ba98c3ffba15bd8fe7
Félix-Antoine Guimont <89605334+FelixAntoine0708@users.noreply.github.com>
2022-09-15 14:50:43 -0400

    end labo 2 + Bonus

---
+++ Labo_2_F.A.Guimont - backup/ItemInventaire.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labo_2_F.A.Guimont
+{
+    class ItemInventaire
+    {
+        private int m_prix;
+        private int m_quantite;
+        private string[] nomAliment = {"Chip BBQ" ,"arachides" ,"Hersey's" ,"M&M" ,"Mars" ,"Fritos" ,"Cheez-it" ,"Pepsi" ,"Jus de Pomme" ,"Jus d'orange" ,"Sprite" ,"Dr. Pepper" ,"Bubble Gum" ,"Welch's" ,"Coca-Cola" ,"eau" ,"Pringles" ,"Jolly Rancher" ,"Nounours en Jujube" ,"Brownie" };
+        private int m_indexFood;
+
+        //Création des propriétés prix et quantite et des méthodes get permettant de lire les variables privées 
+        //m_prix et m_quantite.
+        //Aucune méthode set, car on protège nos variables privées. L'usager devra utiliser les méthodes
+        //modifierInventaire et diminuerInventaire pour modifier les variables
+        public int prix
+        {
+            get
+            {
+                return m_prix;
+            }
+        }
+
+        public int quantite
+        {
+            get
+            {
+                return m_quantite;
+            }
+        }
+
+
+        /// <summary>
+        /// Constructeur. Doit utiliser la méthode modifierInventaire pour assigner les prix et quantite
+        /// </summary>
+        /// <param name="prixItem"> on reçoit le prix de l'item lors de sa création</param>
+        /// <param name="qteItem"> on reçoit la quantité de l'item lors de sa création<</param>
+        public ItemInventaire(int prixItem, int qteItem, int nameFood)
+        {
+
+            m_prix = prixItem;
+            m_quantite = qteItem;
+            m_indexFood = nameFood;
+        }
+
+
+
+        /// <summary>
+        /// Permet de modifier le prix et la quantité d'un item de l'inventaire s'ils sont valides
+        /// </summary>
+        /// <param name="oPrix">Doit être entre 0.25$ et 3.00$ et un multiple de 5 cents</param>
+        /// <param name="oQuantite">Doit être entre 1 et 9 inclusivement</param>
+        /// <returns>Vrai si la modification a réussi. Faux si non.</returns>
+        public bool modifierInventaire(int oPrix, int oQuantite)
+        {
+            int quant = 0;
+            int price = 0;
+            int result = 0;
+
+            if (oQuantite >= 1 && oQuantite <= 9)
+            {
+                quant = 1;
+                if (oPrix >= 25 && oPrix <= 300)
+                {
+                    if ((oPrix % 5)==0)
+                        price = 1;
+                }
+            }
+            result = quant + price;
+
+
+            if (result == 2)
+                return true;
+                
+            else
+                return false;
+                
+        }
+
+        /// <summary>
+        /// Moins un sur la qte si > 0
+        /// </summary>
+        public void diminuerInventaire()
+        {
+            if(m_quantite > 0)
+                m_quantite--;
+
+            else
+                m_quantite = 0;
+        }
+
+        public string getNomAliment()
+        {
+            return nomAliment[m_indexFood];
+        }
+    }
+}
