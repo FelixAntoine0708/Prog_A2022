c8fb9e83216867178f5ceb0dd47b9e2f9539c371
FelixDuck <89605334+FelixAntoine0708@users.noreply.github.com>
2022-10-10 10:39:51 -0400

    comment on labo3 and start of labo4

---
+++ Labo4_StationMeteo_F.A.Guimont/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Labo4_StationMeteo_F.A.Guimont
+{
+    public partial class Form1 : Form
+    {
+        public delegate void monProtoDelegate(string ligneRecu); //étape #1: définir prototype de fonction... paramètres d'entrée et de retour
+        monProtoDelegate objDelegate;  //étape #2: on se déclare un objet delegate. (i.e. un pointeur de fonction ayant ce prototype)
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        List<byte> m_lstTrameRx;
+        const Byte SOH = 0x01;
+
+        public Form1()
+        {
+            InitializeComponent();
+            objDelegate = methodeDelegeAffiche; //avoir les threads
+            serialPort.Encoding = Encoding.GetEncoding(28591);    //pour avoir les accents
+
+            affiche_Com();
+        }
+
+        private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int count = 13;
+
+            while(serialPort.BytesToRead > 0)
+            {
+                try
+                {
+
+                }
+                serialPort.Close();
+                for (int i = 0; i < serialPort.BytesToRead; i++)
+                    serialPort.Read(m_lstTrameRx, i, count);
+            }
+        }
+
+        private void methodeDelegeAffiche(string stLigne)
+        {
+
+        }
+
+        private void affiche_Com()
+        {
+            if (serialPort.IsOpen) // si le port est ouvert
+            {
+                toolStripStatusLabel1.Text = serialPort.PortName + ':' + serialPort.BaudRate + ',' + serialPort.Parity + ',' + serialPort.DataBits + ',' + serialPort.StopBits;   //affiche les informations importantes du port 
+                toolStripStatusLabel2.Text = "Open";    // affiche qu'il est ouvert
+                toolStripStatusLabel2.ForeColor = Color.Green;  // met le mot Open en Vert
+            }
+
+            else    // si le port n'est pas ouvert
+            {
+                toolStripStatusLabel1.Text = serialPort.PortName + ':' + serialPort.BaudRate + ',' + serialPort.Parity + ',' + serialPort.DataBits + ',' + serialPort.StopBits;
+                toolStripStatusLabel2.Text = "Close";   // affiche qu'il est fermer
+                toolStripStatusLabel2.ForeColor = Color.Red;    // met le mot close en rouge
+            }
+
+        }
+
+        private void portToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();   // ferme le port
+            affiche_Com();  // rafraichit l'affichage de l'état du port
+
+            frmConfigPort fConfig = new frmConfigPort(serialPort.PortName, serialPort.BaudRate, serialPort.DataBits, serialPort.Parity, (int)serialPort.StopBits);    // crée une variable instancie vers frmConfigPort
+
+            if (fConfig.ShowDialog() == DialogResult.OK) //Note: La propriété DialogResult du bouton ok doit être à OK.
+            {
+                //On récupère les informations de configurations et on les assignent à au port série
+                if (!(fConfig.m_nom == null))       // s'il y a un port choisi
+                    serialPort.PortName = fConfig.m_nom;  // met le nom du port dans port.PortName
+
+                else    //s'il n'y a pas de port choisi
+                    MessageBox.Show("aucun com à été choisi");
+
+                serialPort.BaudRate = fConfig.m_vitesse;  // met la vitesse dans le baudrate 
+                serialPort.DataBits = fConfig.m_nbBit;    // met le nombre de bits dans le dataBits 
+                serialPort.Parity = fConfig.m_parite; // met la parité dans le parity 
+                serialPort.StopBits = (System.IO.Ports.StopBits)fConfig.m_stopBit;   // met le stop bits dans une variable 
+
+                try
+                {
+                    serialPort.Open();    // ouvre le port
+                    affiche_Com();
+                }
+
+                catch
+                {
+                    MessageBox.Show("Ne peux pas ouvrir " + serialPort.PortName);  // affiche un message si nous pouvons pas ouvrir sur le port
+                }
+            }
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void enregistrerSousToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text files (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            enregistrerSousToolStripMenuItem.PerformClick();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            portToolStripMenuItem.PerformClick();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Labo4_StationMeteo_F.A.Guimont/frmConfigPort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labo4_StationMeteo_F.A.Guimont
+{
+    public partial class frmConfigPort : Form
+    {
+        // variable membres de la classe frmConfigPort
+        public string m_nom { get; set; }
+        public int m_vitesse { get; set; }
+        public int m_nbBit { get; set; }
+        public Parity m_parite { get; set; }
+        public int m_stopBit { get; set; }
+
+        public frmConfigPort(string PortName, int BaudRate, int DataBits, Parity parity, int StopBits)
+        {
+            InitializeComponent();
+
+            comboPort.DataSource = (System.IO.Ports.SerialPort.GetPortNames()); //met tout les port dans les items de la comboBox
+            comboPort.SelectedItem = PortName;  // affiche celui qui a été importer
+            comboPort.DropDownStyle = ComboBoxStyle.DropDownList;   // met la comboBox en dropDownList
+
+            comboVitesse.DataSource = new int[] { 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200, 128000, 256000 };    // met les vitesse dans les items du comboBox
+            comboVitesse.SelectedItem = BaudRate;   // affiche celui qui a été importer
+            comboVitesse.DropDownStyle = ComboBoxStyle.DropDownList;    // met la comboBox en dropDownList
+
+            comboNbBits.DataSource = new int[] { 7, 8 };    // met les nombres de bits dans les items du comboBox
+            comboNbBits.SelectedItem = DataBits;    // affiche celui qui a été importer
+            comboNbBits.DropDownStyle = ComboBoxStyle.DropDownList; // met la comboBox en dropDownList
+
+            comboParite.DataSource = Enum.GetValues(typeof(Parity));    // met la parité dans les items du comboBox
+            comboParite.SelectedItem = parity;  // affiche celui qui a été importer
+            comboParite.DropDownStyle = ComboBoxStyle.DropDownList; // met la comboBox en dropDownList
+
+            comboStpBits.DataSource = new int[] { 1, 2 };   // met le stop bits dans les items du comboBox
+            comboStpBits.SelectedItem = StopBits;   // affiche celui qui a été importer
+            comboStpBits.DropDownStyle = ComboBoxStyle.DropDownList;    // met la comboBox en dropDownList
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            m_nom = (string)comboPort.SelectedValue;   // envoie la valeur du port dans la vartiable public de la classe
+            m_vitesse = (int)comboVitesse.SelectedValue;   // envoie la valeur de la vitesse dans la vartiable public de la classe
+            m_nbBit = (int)comboNbBits.SelectedValue;  // envoie la valeur de nombre de bits dans la vartiable public de la classe
+            m_parite = (Parity)comboParite.SelectedValue;  // envoie la valeur de la parité dans la vartiable public de la classe
+            m_stopBit = (int)comboStpBits.SelectedValue;   // envoie la valeur du stop bits dans la vartiable public de la classe
+        }
+    }
+}
