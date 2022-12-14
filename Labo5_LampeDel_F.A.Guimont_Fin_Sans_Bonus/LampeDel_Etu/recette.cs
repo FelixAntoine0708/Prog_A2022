9b4cd1510bd11127c92765daf828fd1ebd29235b
FelixDuck <89605334+FelixAntoine0708@users.noreply.github.com>
2022-12-14 13:46:14 -0500

    End of Lab5 without Bonus

---
+++ Labo5_LampeDel_F.A.Guimont_Fin_Sans_Bonus/LampeDel_Etu/recette.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LampeDel_Etu
+{
+    internal class recette
+    {
+        public List<int> del { get; set; } //listes d'intensités des DEL
+        public bool pointEnr { get; set; } //on veut savoir si cette recette a été enregistrée pour l'usager
+
+        /// <summary>
+        /// Constructeur. Construit la liste des DEL. Met à 0 l'intensité de chaque DEL.
+        /// Met à false pointEnr 
+        /// </summary>
+        /// <param name="nb"> nombre de del pour cette recette</param>
+        public recette(int nb)
+        {
+            del = new List<int>();
+
+            for (int i = 0; i < nb; i++)
+                del.Add(0);
+
+            pointEnr = false;
+        }
+    }
+}
